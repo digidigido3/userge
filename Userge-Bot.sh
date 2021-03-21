e5bfd3f1f142dcf8d6b70896385160c369288914
digidigido3 <75541660+digidigido3@users.noreply.github.com>
2021-03-21 19:44:50 +0700

    Create Userge-Bot.sh

---
+++ Userge-Bot.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+sudo apt install tree wget2 p7zip-full jq ffmpeg wget git
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+wget https://repo.juicedama.workers.dev/Userge/Userge-Bot.zip
+unzip ./Userge-Bot.zip
+cd ./Userge
+sudo apt install python3.8
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.8 get-pip.py
+pip3.8 install -r requirements.txt
